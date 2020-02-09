71d733a6211926cad76a19ee6a706666cbedbc11
shohei <s.mukai@creditengine.jp>
2020-02-09 15:20:41 +0900

    Add setup py file.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='django-view-table',
+    version='0.0.1',
+    packages=[''],
+    url='https://github.com/pyohei/django-view-table',
+    license='MIT',
+    author='Shohei Mukai',
+    author_email='mukaishohei76@gmail.com',
+    description=''
+)
