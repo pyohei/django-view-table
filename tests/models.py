b9bbb16bbea766d809fead78d97ce3f5027e0496
shohei <s.mukai@creditengine.jp>
2020-05-28 21:36:47 +0900

    Add model for testing.

--- tests/models.py
+++ tests/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from view_table.models import ViewTable
 
 # Create your models here.
+
+class Author(models.Model):
+    name = models.CharField(max_length=50)
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
