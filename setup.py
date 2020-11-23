a055008bdce0dd0bd4d514ee860e671dbe3ce662
shohei <s.mukai@creditengine.jp>
2020-11-23 12:15:26 +0900

    Fix description.

--- setup.py
+++ setup.py
@@ -2,11 +2,13 @@ from setuptools import setup
 
 setup(
     name='django-view-table',
-    version='1.0.0',
+    version='1.0.1',
     packages=['view_table', 'view_table.management.commands'],
     url='https://github.com/pyohei/django-view-table',
     license='MIT',
     author='Shohei Mukai',
     author_email='mukaishohei76@gmail.com',
-    description=''
+    description='Django view table plugin',
+    long_description=open('README.md', 'r').read(),
+    long_description_content_type='text/markdown',
 )
