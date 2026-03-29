9acc3a97c87689caa2251b14da283f3de438fbcc
pyohei <mukaishohei76@gmail.com>
2026-03-29 13:16:45 +0900

    🔨 Fix setup scripts.

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ setup(
     name="django-view-table",
     version="1.0.1",
     packages=["view_table", "view_table.management.commands"],
+    install_requires=["Django>=4.2,<5.0"],
     url="https://github.com/pyohei/django-view-table",
     license="MIT",
     author="Shohei Mukai",
